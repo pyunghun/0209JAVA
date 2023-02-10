fd1df6b7618ff66bd8a2c4227490062a303cdbf8
pyunghun cho <pyunghun120@gmail.com>
2023-02-10 18:21:22 +0900

    안녕하세요

---
+++ src/com/greedy/section01/connection/Application.java
@@ -0,0 +1,38 @@
+package com.greedy.section01.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		Connection con = null;
+		
+		try {
+			/* 사용할 드라이버 등록 */
+			Class.forName("oracle.jdbc.driver.OralcleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "C##EMPLOYEE", "EMPLOYEE");
+			
+			System.out.println(con);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(con != null)
+			{
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+	}
+
+}
